1db25438716dbea36500887ac692402c03febf2f
Egor Domnin <zombie9@inbox.ru>
2016-12-01 12:18:37 +0300

    Reworked aspline a bit, changed tabletcanvas for correct resize so we won't lose data

---
+++ testform/aspline.h
@@ -0,0 +1,8 @@
+#ifndef ASPLINE_H
+#define ASPLINE_H
+
+#endif // ASPLINE_H
+
+void computeXlimits(QVector<mPoint>* points,int s, int e, qreal *llimit, qreal *ulimit);
+
+void calcspline(QVector<mPoint>* points, QVector<mPoint>* outp, int d);
